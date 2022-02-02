1df9971411d13101202d8023674ff9af8d8caa21
Campbell Gilmore <boss2020mlg@gmail.com>
2022-02-02 08:56:35 -0600

    lol

---
+++ crash.js
@@ -0,0 +1,7 @@
+document.getElementById("crashbutton").onclick = function() {
+var t = "lol";
+while(1) {
+    t+="lol";
+}
+// bwahahahahah all of your memory is gone and you will now crash!!!! hahahaha!!!!
+}
