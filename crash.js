2877931f811dffb528654f74b649ae7701510ba1
Campbell Gilmore <boss2020mlg@gmail.com>
2022-02-02 09:08:01 -0600

    f

--- crash.js
+++ crash.js
@@ -1,9 +1,8 @@
-document.getElementById("crashbutton").onclick = function() {
-console.log("funny activated")
-document.getElementById("crashbutton").style = "background-color: blue"
-var t = "lol";
-while(1) {
-    t+="lol";
-}
-// bwahahahahah all of your memory is gone and you will now crash!!!! hahahaha!!!!
+document.getElementById("crashbutton").onclick = crash();
+function crash() {
+    document.getElementById("crashbutton").style = "background-color: blue"
+    var t = "lol";
+    while(1) {
+        t+="lol";
+    }
 }
